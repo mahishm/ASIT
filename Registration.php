1b7097b8dd8aca12a75b139044809b9d07531fe1
rhennessey2019 <39124735+rhennessey2019@users.noreply.github.com>
2018-05-15 12:31:19 -0400

    categorized more files

--- Registration.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<?php 	var_dump($GLOBALS) ?>
-Code: <?php echo $_REQUEST["Code"]; ?><br>
-Password <?php echo $_REQUEST["Password"]; ?><br>
-Confirmation Password <?php echo $_REQUEST["PasswordCheck"]; ?>
-</body>
-</html>
--- htdocs/Registration.php
+++ htdocs/Registration.php
@@ -2,7 +2,7 @@
 <body>
 <?php 	var_dump($GLOBALS) ?>
 Code: <?php echo $_REQUEST["Code"]; ?><br>
-Password <?php echo $_REQUEST["Password"]; ?>
+Password <?php echo $_REQUEST["Password"]; ?><br>
 Confirmation Password <?php echo $_REQUEST["PasswordCheck"]; ?>
 </body>
 </html>
